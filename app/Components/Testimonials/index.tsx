826d88f2c49127290616e8cb20cc584a60a91343
Azhar Khan <119923339+azharkhan93@users.noreply.github.com>
2024-01-21 23:37:49 +0530

    new changes

--- app/Components/Testimonials/index.tsx
+++ app/Components/Testimonials/index.tsx
@@ -5,18 +5,17 @@ const TestimonialSection: React.FC = () => {
   return (
     <section className="bg-gray-100 py-11 md:flex items-center" style={{ backgroundImage: "url('/bgtest.png')", backgroundSize: "cover",  }}>
         
-      <div className=" container mx-auto flex flex-col md:flex-row h-[400px]">
+      <div className=" container mx-auto flex flex-col md:flex-row h-[430px]">
       
         
         
 
-        {/* Testimonial Image */}
         <div className="md:w-1/2 md:pl-8 relative">
           <h2 className="text-4xl font-bold mb-4">Quote From A Satisfied Customer</h2>
           <p className="text-gray-700 mb-6">
             Lorem Ipsum is simply dummy text
           </p>
-          <div className="relative w-full h-80 mb-6 rounded-2xl top-8">
+          <div className="relative w-full h-80 mb-6 rounded-2xl top-10">
             <Image
               src="/person.png"
               alt="Testimonial Image"
@@ -25,6 +24,15 @@ const TestimonialSection: React.FC = () => {
             />
           </div>
         </div>
+        {/* <div className=" md:overflow-hidden md:mr-4 md:mt-10 bg-black">
+          <Image
+             src="/skull.png"  
+             alt="Small Image 2"
+             width={200}
+             height={100}
+             objectFit="cover"
+          />
+        </div> */}
         
         
 
@@ -33,7 +41,7 @@ const TestimonialSection: React.FC = () => {
         
   
   <div className="md:w-1/2 md:pl-8">
-  <div className="relative w-16 h-16 rounded-full overflow-hidden mr-4 left-20 mb-11" >
+  <div className="relative w-16 h-16 rounded-full overflow-hidden mr-4 left-32 mb-20" >
               <Image
                 src="/lego.png"  
                 alt="Small Image"
@@ -73,10 +81,10 @@ const TestimonialSection: React.FC = () => {
   </div>
   
   <div className="text-center">
-    {/* Reviewer's Name */}
+  
     <p className="text-l">Dr. John Smith</p>
 
-    {/* Client's Role */}
+    
     <p className="text-gray-500 text-sm">Chief of Radiology at XYZ Hospital</p>
   </div>
 </div>
@@ -108,9 +116,7 @@ const TestimonialSection: React.FC = () => {
         "The accuracy of Medical Imaging Solutions' technology has revolutionized the way I practice medicine."
       </p>
 
-      {/* <p className="text-gray-700 mb-6">
-      "The accuracy of Medical Imaging Solutions' technology has revolutionized the way I practice medicine."
-      </p> */}
+    
       <div className="flex flex-col items-center -mt-4 ">
   <div className="relative w-10 h-10 rounded-full overflow-hidden mb-2">
     <Image src="/tr.png" alt="Reviewer Avatar" layout="fill" objectFit="cover" />
@@ -130,30 +136,9 @@ const TestimonialSection: React.FC = () => {
     
   </div>
 </div>
-{/* <div className="relative  rounded-full mb-8">
-    <Image
-        
-        src="/skull.png"  
-        alt="Small Image 2"
-        width={200}
-        height={100}
-        objectFit="cover"
-        
-    />
-    
-</div> */}
 
-{/* <div className="  flex justify-end items-end">
 
-    <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M19 26.2L11.8 19M11.8 19L19 11.8M11.8 19H26.2M1 19C1 28.9411 9.05887 37 19 37C28.9411 37 37 28.9411 37 19C37 9.05887 28.9411 1 19 1C9.05887 1 1 9.05887 1 19Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>
 
-    <svg width="38" height="38" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M19 26.2L26.2 19M26.2 19L19 11.8M26.2 19H11.8M37 19C37 28.9411 28.9411 37 19 37C9.05887 37 1 28.9411 1 19C1 9.05887 9.05887 1 19 1C28.9411 1 37 9.05887 37 19Z" stroke="#F5756B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-    </svg>
-   
-  </div> */}
    </div>
    
       
@@ -163,3 +148,19 @@ const TestimonialSection: React.FC = () => {
 };
 
 export default TestimonialSection;
+
+
+
+{/* <div className="relative  rounded-full mb-8 bg-black overflow-hidden">
+    <Image
+        
+        src="/skull.png"  
+        alt="Small Image 2"
+        width={200}
+        height={100}
+        objectFit="cover"
+        
+        
+    />
+    
+</div> */}
